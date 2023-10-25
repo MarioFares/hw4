39198645456b10920355bd5b95eba517862ef4b4
mario <mariofares02@gmail.com>
2023-10-25 16:50:19 -0400

    Tests for if statements

--- test/studenttests.ml
+++ test/studenttests.ml
@@ -6,9 +6,14 @@ open Gradedtests
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
+(* Tests marked as easy were used in early stages of the        *)
+(* compiler. Most have a single function, the program function  *)
 (* These tests have been used for early stages of building the  *)
 (* compiler to ensure the correctness along the way. We are     *)
 (* returning expression. *)
+(* Note the tests are valid even at the final step of finishing *)
+(* the compiler. They are just used to ensure things are right  *)
+(* before implementing more features. *)
 let easy_ret_tests = [
   ("hw4programs/my_tests/ret_add.oat",  "", "20")
 ; ("hw4programs/my_tests/ret_sub.oat",  "", "10")
@@ -42,9 +47,36 @@ let easy_assign_tests = [
 ; ("hw4programs/my_tests/assign_5.oat", "", "40")
 ]
 
+let easy_if_tests = [
+  (* Testing True & False as the condition *)
+  ("hw4programs/my_tests/if_1.oat", "", "12")
+; ("hw4programs/my_tests/if_2.oat", "", "10")
+; ("hw4programs/my_tests/if_3.oat", "", "20")
+; ("hw4programs/my_tests/if_4.oat", "", "5")
+  (* Testing Different Comparison Operators *)
+; ("hw4programs/my_tests/if_5.oat", "", "30")
+; ("hw4programs/my_tests/if_6.oat", "", "30")
+; ("hw4programs/my_tests/if_7.oat", "", "30")
+; ("hw4programs/my_tests/if_8.oat", "", "30")
+; ("hw4programs/my_tests/if_9.oat", "", "20")
+; ("hw4programs/my_tests/if_10.oat", "", "30")
+  (* Testing If-Else If-Else Branches Work *)
+; ("hw4programs/my_tests/if_11.oat", "", "20")
+; ("hw4programs/my_tests/if_12.oat", "", "40")
+; ("hw4programs/my_tests/if_13.oat", "", "31")
+; ("hw4programs/my_tests/if_14.oat", "", "30")
+; ("hw4programs/my_tests/if_15.oat", "", "5")
+  (* Testing &, |, ! in Compound Conditiondal *)
+; ("hw4programs/my_tests/if_16.oat", "", "5")
+; ("hw4programs/my_tests/if_17.oat", "", "10")
+; ("hw4programs/my_tests/if_18.oat", "", "50")
+; ("hw4programs/my_tests/if_19.oat", "", "5")
+]
+
 let provided_tests : suite = [
   Test (">>> easy ret tests", executed_oat_file easy_ret_tests);
   Test (">>> easy call tests", executed_oat_file easy_call_tests);
   Test (">>> easy decl tests", executed_oat_file easy_decl_tests);
   Test (">>> easy assign tests", executed_oat_file easy_assign_tests);
+  Test (">>> easy if tests", executed_oat_file easy_if_tests);
 ] 
